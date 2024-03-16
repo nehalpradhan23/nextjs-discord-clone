45d8b0cda41cd686a7520b4a713990f872875b1d
Nehal pradhan <40754890+nehalpradhan23@users.noreply.github.com>
2024-03-16 20:51:14 +0530

    clerk authentication and dark mode

--- app/(main)/(routes)/page.tsx
+++ app/(main)/(routes)/page.tsx
@@ -1,7 +1,11 @@
+import { ModeToggle } from "@/components/mode-toggle";
+import { UserButton } from "@clerk/nextjs";
+
 export default function Home() {
   return (
     <div className="text-3xl font-bold text-indigo-500">
-      This is a protected route
+      <UserButton afterSignOutUrl="/" />
+      <ModeToggle />
     </div>
   );
 }
