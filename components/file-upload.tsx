9fa68dcdc7aceb93a324b1d641061783f8368cbe
Nehal pradhan <40754890+nehalpradhan23@users.noreply.github.com>
2024-03-28 02:44:01 +0530

    file attachment, send image or pdf

--- components/file-upload.tsx
+++ components/file-upload.tsx
@@ -3,7 +3,7 @@
 import { UploadDropzone } from "@/lib/uploadthing";
 import "@uploadthing/react/styles.css";
 import { error } from "console";
-import { X } from "lucide-react";
+import { FileIcon, X } from "lucide-react";
 import Image from "next/image";
 
 interface FileUploadProps {
@@ -17,6 +17,7 @@ export const FileUpload = ({ onChange, value, endpoint }: FileUploadProps) => {
   if (value && fileType !== "pdf") {
     return (
       <div className="relative h-20 w-20">
+        {/* show uploaded image ================ */}
         <Image
           fill
           src={value}
@@ -34,6 +35,32 @@ export const FileUpload = ({ onChange, value, endpoint }: FileUploadProps) => {
       </div>
     );
   }
+
+  // show pdf upload ==============================================
+  if (value && fileType === "pdf") {
+    return (
+      <div className="relative flex items-center p-2 mt-2 rounded-md bg-background/10">
+        <FileIcon className="h-10 w-10 fill-indigo-200 stroke-indigo-400" />
+        <a
+          href={value}
+          target="_blank"
+          rel="noopener noreferrer"
+          className="ml-2 text-sm text-indigo-500 dark:text-indigo-400 hover:underline"
+        >
+          {value}
+        </a>
+        {/* remove pdf ============================= */}
+        <button
+          onClick={() => onChange("")}
+          className="bg-rose-500 text-white p-1 rounded-full absolute -top-2 -right-2 shadow-sm hover:bg-rose-800"
+          type="button"
+        >
+          <X className="h-4 w-4" />
+        </button>
+      </div>
+    );
+  }
+  // ==========================================================
   return (
     <UploadDropzone
       endpoint={endpoint}
